ae6c6fb37663147edca609346012784f745e80ff
Simon <github@simonmesserli.ch>
2021-10-22 18:42:56 +0200

    change to spot

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,11 @@
 resource "aws_ecs_cluster" "ybhackathon" {
   name = "ybhackathon-2021"
+
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]  
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
 }
 
 resource "aws_ecr_repository" "ybhackathon" {
